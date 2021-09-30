a16c531f7a1cff51f1bc5e5103aed8135c426a2e
Faadhil <faadhil.aa@gmail.com>
2021-09-29 21:38:17 -0400

    add Interview and Appointment components

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classNames from "classnames"
+
+import "./InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+const {name, avatar, onChange} = props;
+
+const interviewerClass = classNames("interviewers__item", {"interviewers__item--selected": props.selected})
+
+  return (
+    <li className={interviewerClass} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={avatar}
+        alt={name}
+      />
+      {props.selected && name}
+    </li>
+  );
+}
